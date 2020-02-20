b4e379ffb04d153271d43cf10be412a36b7d4ddf
Marcin Kozubek <mkozubek@protonmail.com>
2020-02-20 10:40:32 +0100

    Basic activity layouts

--- app/src/main/java/online/kozubek/czoleczko/MainActivity.kt
+++ app/src/main/java/online/kozubek/czoleczko/MainActivity.kt
@@ -1,12 +1,22 @@
 package online.kozubek.czoleczko
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var playButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playButton = findViewById(R.id.play)
+
+        playButton.setOnClickListener {
+            startActivity(Intent(this, QuestionPackagesActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/online/kozubek/czoleczko/QuestionPackagesActivity.kt
@@ -0,0 +1,12 @@
+package online.kozubek.czoleczko
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionPackagesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_packages)
+    }
+}
